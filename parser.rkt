cd769779b2a876b29aba209ddf161ad298f2d59f
Ellen Ouyang <ouyel-26@rhodes.edu>
2026-04-13 19:40:23 -0500

    updating grammar to Gerardo's request

--- parser.rkt
+++ parser.rkt
@@ -1,13 +1,22 @@
 #lang brag
+program      : section*
+@section     : room | characters
 
-command : verb-phrase 
-        | verb-phrase noun-phrase
-        | verb-phrase noun-phrase prep noun-phrase
+room         : ROOM /LBRACE room-property-pair* /RBRACE
+room-property-pair: room-property /COLON value
+room-property: NAME | LINKS | SIZE
+size         : list
 
-verb-phrase : VERB
+characters    : NAME | list | dialogue | quests
 
-noun-phrase : NOUN
-            | ARTICLE NOUN
-            | ARTICLE NOUN NOUN
+items          : list
 
-prep : PREP
+dialogue       : STRING | list
+
+quests         : list
+
+
+value        : STRING | list 
+number       : INT | FLOAT
+@list        : /LBRACKET list-items /RBRACKET
+@list-items  : [value] (COMMA value)*
